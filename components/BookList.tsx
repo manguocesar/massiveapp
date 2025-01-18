3125046f4e7039e78dedd55b8f4d93557bf3f962
Cesar <cesar.hertz@icloud.com>
2025-01-18 18:26:34 +0100

    Landing page

---
+++ components/BookList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookList = (title) => {
+    return (
+        <section>
+            <h2 className='font-bebas-neue text-4xl text-light-100'>{title.title}</h2>
+        </section>
+    )
+}
+
+export default BookList
---
+++ types.d.ts
@@ -0,0 +1,16 @@
+interface Book {
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  totalCopies: number;
+  availableCopies: number;
+  description: string;
+  coverColor: string;
+  coverUrl: string;
+  video: string;
+  summary: string;
+  isLoanedBook: boolean;
+  id: number;
+  userId: number;
+}
