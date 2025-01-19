04c6ba0877e0bc8bc511bed11e00b9b53a622311
Cesar <cesar.hertz@icloud.com>
2025-01-19 19:15:33 +0100

    Add database migration setup and schema definitions for users, books, and borrow records

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,18 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "@/constants";
-// import { db } from "@/database/drizzle";
-// import { books, users } from "@/database/schema";
+import { users } from "@/database/schema";
+import { db } from "@/database/drizzle";
 // import { auth } from "@/auth";
 // import { desc } from "drizzle-orm";
 
 const Home = async () => {
   // const session = await auth();
+
+  const result = await db.select().from(users)
+
+  console.log("result",result);
+  
 
   // const latestBooks = (await db
   //   .select()
