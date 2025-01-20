dbc99e105aaa14b2fb9e590b7d137982d2e8bd08
Cesar <cesar.hertz@icloud.com>
2025-01-20 10:43:36 +0100

    Add rate limiting functionality and create a temporary pause page; integrate Redis for rate limiting

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -7,10 +7,10 @@ import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
 import { headers } from "next/headers";
-// import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
 // import { workflowClient } from "@/lib/workflow";
 import config from "@/lib/config";
+import ratelimit from "../ratelimit";
 
 export const signInWithCredentials = async (
     // Pick is a utility type that allows you to create a new type by picking the properties from an existing type.
@@ -18,10 +18,12 @@ export const signInWithCredentials = async (
 ) => {
   const { email, password } = params;
 
+  // to get the ip address of the user
   const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
-  //   const { success } = await ratelimit.limit(ip);
+  //can go to that page successfully
+    const { success } = await ratelimit.limit(ip);
 
-  //   if (!success) return redirect("/too-fast");
+    if (!success) return redirect("/too-fast");
 
   try {
     const result = await signIn("credentials", {
@@ -45,9 +47,9 @@ export const signUp = async (params: AuthCredentials) => {
   const { fullName, email, universityId, password, universityCard } = params;
 
   const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
-  //   const { success } = await ratelimit.limit(ip);
+    const { success } = await ratelimit.limit(ip);
 
-  //   if (!success) return redirect("/too-fast");
+    if (!success) return redirect("/too-fast");
 
   const existingUser = await db
     .select()
