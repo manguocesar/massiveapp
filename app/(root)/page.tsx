f370914dfa16669465b7c00b92bf0eddbacbac40
Cesar <cesar.hertz@icloud.com>
2025-01-22 18:27:38 +0100

    feat(BookDetails): create page for individual book details with video and summary components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -15,25 +15,6 @@ const Home = async () => {
     .limit(10)
     .orderBy(desc(books.createdAt))) as Book[];
 
-    const getRandomInt = (min: number, max: number) => {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    let oneToSixInt = getRandomInt(1, 6);
-
-    const shuffleArray = (array: any[]) => {
-      for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
-    }
-
-    const shuffledBooks = shuffleArray([...sampleBooks]);
-
-
   return (
     <>
       <BookOverview 
